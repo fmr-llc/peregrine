9b893f043ab4048c68a5548c876d8ea44b4af702
Robert Coords <Robert.Coords@fmr.com>
2015-10-15 08:14:42 -0400

    Updated web.xml to support POST Rest Calls. Corrected issues with broker-destination.properties. Fixed comments with SecurityTests.
    Fidelity-DCO-1.1-Signed-off-by: Robert Coords<Robert.Coords@fmr.com>(github: robertcoords)

--- tests/com/alliancefoundry/tests/security/SecurityTests.java
+++ tests/com/alliancefoundry/tests/security/SecurityTests.java
@@ -22,26 +22,24 @@ public class SecurityTests {
 	@Test
 	public void testAccessRestServiceWithoutPassword() throws ClientProtocolException, IOException {
 		
-		// access get event reest service
+		// access get event rest service
 		CloseableHttpClient httpclient = HttpClients.createDefault();
 		HttpGet httpGet = new HttpGet("http://user:password@localhost:8080/event-service/event/34578");
 		CloseableHttpResponse response1 = httpclient.execute(httpGet);
 
 		// Status code 401 - Unauthorized 401 
 		Assert.assertTrue((response1.getStatusLine().getStatusCode() == 401));
-	
 	}
 
 	@Test
 	public void testAccessRestServiceWithPassword() throws ClientProtocolException, IOException {
 		
-		// access get event reest service
-
+		// access get event rest service
 		CloseableHttpClient httpclient = HttpClients.createDefault();
 		HttpGet httpGet = new HttpGet("http://user:user@localhost:8080/event-service/event/34578");
 		CloseableHttpResponse response1 = httpclient.execute(httpGet);
 				
-		// Status code 401 - Unauthorized 401 
+		// Status code 200 or 201 - Correct Authorization 
 		Assert.assertTrue((response1.getStatusLine().getStatusCode() == 200) || ((response1.getStatusLine().getStatusCode() == 201)));
 	}
 
