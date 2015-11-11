0a72ece22fb13c1346059fffa9026602f314eaf9
Paul Bernard <paul.bernard@fmr.com>
2015-11-11 10:23:56 -0500

    Signed-off-by: Paul Bernard <paul.bernard@fmr.com>
    Initial Commit of Refactored code base to support Spring Boot & Swagger Support

---
+++ src/main/java/com/alliancefoundry/Boot.java
@@ -0,0 +1,108 @@
+package com.alliancefoundry;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.fasterxml.classmate.TypeResolver;
+import io.swagger.annotations.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.async.DeferredResult;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.schema.WildcardType;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.mappers.LicenseMapper;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static springfox.documentation.schema.AlternateTypeRules.newRule;
+
+/**
+ * Created by Paul Bernard on 10/21/15.
+ *  org.springframework.boot.actuate.autoconfigure.ManagementSecurityAutoConfiguration.class,
+ */
+@SpringBootApplication
+@EnableSwagger2
+@EnableAutoConfiguration(exclude = {
+        org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.ManagementSecurityAutoConfiguration.class,
+        com.alliancefoundry.controller.EventServiceController.class})
+@ImportResource({"classpath*:eventservice-servlet.xml",
+                "classpath*:broker-config.xml",
+                "classpath*:db-config.xml"})
+public class Boot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot.class, args);
+    }
+
+    @Bean
+    public Docket petApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build().pathMapping("/")
+                .apiInfo(new ApiInfo(
+                                "Peregrine Event Service",
+                                "Peregrine Event Service is a canonical implementation of the Event store pattern",
+                                "0.0.1",
+                                "",
+                                "Alliance Foundry",
+                                "Apache 2.0",
+                                "http://www.apache.org/licenses/LICENSE-2.0.html"))
+                .directModelSubstitute(LocalDate.class, String.class).genericModelSubstitutes(ResponseEntity.class)
+                .alternateTypeRules(newRule(typeResolver.resolve(DeferredResult.class, typeResolver.resolve(ResponseEntity.class, WildcardType.class)), typeResolver.resolve(WildcardType.class)))
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.GET, newArrayList(new ResponseMessageBuilder().code(500).message("500 message").responseModel(new ModelRef("Error")).build()));
+                //.securitySchemes(newArrayList(apiKey())).securityContexts(newArrayList(securityContext()));
+    }
+
+    @Autowired
+    private TypeResolver typeResolver;
+
+    /*
+    private ApiKey apiKey() {
+        return new ApiKey("mykey", "api_key", "header");
+    }
+
+
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).forPaths(PathSelectors.regex("/echo.*")).build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return newArrayList(new SecurityReference("mykey", authorizationScopes));
+    }
+
+
+    @Bean
+    SecurityConfiguration security() {
+        return new SecurityConfiguration("test-app-client-id", "test-app-realm", "test-app", "apiKey");
+    }*/
+
+
+    @Bean
+    UiConfiguration uiConfig() {
+        return new UiConfiguration("validatorUrl");
+    }
+
+
+}
